5f84e9a66792fc2a4a058c5918cdbd8656fbc74c
David Stevenson <drstevenson1958@yahoo.com>
2023-10-28 11:57:29 -0400

    Listing 3-42. Restricting Access in the Edit.cshtml.cs file in the Pages folder

--- ch03/IdentityApp/IdentityApp/Pages/Edit.cshtml.cs
+++ ch03/IdentityApp/IdentityApp/Pages/Edit.cshtml.cs
@@ -1,9 +1,11 @@
 using IdentityApp.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityApp.Pages
 {
+    [Authorize(Roles = "Admin")]
     public class EditModel : PageModel
     {
         public EditModel(ProductDbContext ctx) => DbContext = ctx;
