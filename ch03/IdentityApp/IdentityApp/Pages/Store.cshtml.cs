c997956cc574e95d7808ed1596ebdc8a97b8a5b6
David Stevenson <drstevenson1958@yahoo.com>
2023-10-28 11:54:30 -0400

    Listing 3-39. Restricting access in the Store.cshtml.cs file in the Pages folder

--- ch03/IdentityApp/IdentityApp/Pages/Store.cshtml.cs
+++ ch03/IdentityApp/IdentityApp/Pages/Store.cshtml.cs
@@ -1,9 +1,10 @@
 using IdentityApp.Models;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace IdentityApp.Pages
 {
+    [Authorize]
     public class StoreModel : PageModel
     {
         public StoreModel(ProductDbContext ctx) => DbContext = ctx;
