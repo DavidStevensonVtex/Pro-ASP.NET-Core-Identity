c300689e59450ca90802e2b5bcc7b02f981826f5
David Stevenson <drstevenson1958@yahoo.com>
2023-10-29 16:59:58 -0400

    Listing 5-4. SignIn Confirmations for Email, Account and Phone Number in the Program.cs file

--- ch05/IdentityApp/IdentityApp/Program.cs
+++ ch05/IdentityApp/IdentityApp/Program.cs
@@ -38,6 +38,9 @@ namespace IdentityApp
                     opts.Password.RequireDigit = false;
                     opts.Password.RequireLowercase = false;
                     opts.Password.RequireNonAlphanumeric = false;
+                    opts.SignIn.RequireConfirmedEmail = false;          // default: false
+                    opts.SignIn.RequireConfirmedAccount = true;         // default: false
+                    opts.SignIn.RequireConfirmedPhoneNumber = false;    // default: false
                 })
                 .AddEntityFrameworkStores<IdentityDbContext>();
 
