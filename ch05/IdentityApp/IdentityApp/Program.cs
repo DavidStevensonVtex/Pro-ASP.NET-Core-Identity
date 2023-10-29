beb97b33ae9f1e4c9165a336b988b1ed303a382a
David Stevenson <drstevenson1958@yahoo.com>
2023-10-29 16:44:42 -0400

    Chapter 5: Configuring Identity

---
+++ ch05/IdentityApp/IdentityApp/Program.cs
@@ -0,0 +1,67 @@
+using IdentityApp.Models;
+using IdentityApp.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ProductDbContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration["ConnectionStrings:AppDataConnection"]);
+                //opts.EnableSensitiveDataLogging(true);
+            });
+
+            builder.Services.AddDbContext<IdentityDbContext>(opts =>
+            {
+                opts.UseSqlServer(
+                    builder.Configuration["ConnectionStrings:IdentityConnection"],
+                    opts => opts.MigrationsAssembly("IdentityApp"));
+            });
+
+            builder.Services.AddScoped<IEmailSender, ConsoleEmailSender>();
+
+            builder.Services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<IdentityDbContext>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //    endpoints.MapRazorPages();
+            //});
+
+            app.MapDefaultControllerRoute();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
