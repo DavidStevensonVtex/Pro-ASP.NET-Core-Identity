3141ffc23b85dbc7ff0232e6937947c9802aaafd
David Stevenson <drstevenson1958@yahoo.com>
2023-10-29 12:13:51 -0400

    Chapter 4: Using the Identity UI Package

---
+++ ch04/IdentityApp/IdentityApp/Program.cs
@@ -0,0 +1,51 @@
+using IdentityApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+            builder.Services.AddDbContext<ProductDbContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration["ConnectionStrings:AppDataConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapDefaultControllerRoute();
+            //    endpoints.MapRazorPages();
+            //});
+
+            app.MapDefaultControllerRoute();
+            app.MapRazorPages();
+
+            app.Run();
+        }
+    }
+}
