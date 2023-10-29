41f7254423ad7760959a4d24b82229598a538aab
David Stevenson <drstevenson1958@yahoo.com>
2023-10-29 14:29:42 -0400

    Listing 4-13. The Contents of the ConsoleEmailSender.cs file in the Services folder

---
+++ ch04/IdentityApp/IdentityApp/Services/ConsoleEmailSender.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace IdentityApp.Services
+{
+    public class ConsoleEmailSender
+    {
+        public Task SendEmailAsync(string emailAddress, string subject, string htmlMessage)
+        {
+            Console.WriteLine("--- New Email ---");
+            Console.WriteLine($"To: {emailAddress}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine(HttpUtility.HtmlDecode(htmlMessage));
+            Console.WriteLine("---------");
+            return Task.CompletedTask;
+        }
+    }
+}
