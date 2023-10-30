d321d84073226f7b66abce61a54408d4ed0f0244
David Stevenson <drstevenson1958@yahoo.com>
2023-10-30 14:30:47 -0400

    Chapter 6: Adapting Identity UI

---
+++ ch06/IdentityApp/IdentityApp/Services/ConsoleEmailSender.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace IdentityApp.Services
+{
+    public class ConsoleEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string emailAddress, string subject, string htmlMessage)
+        {
+            Console.WriteLine("--- New Email ---");
+            Console.WriteLine($"To: {emailAddress}");
+            Console.WriteLine($"Subject: {subject}");
+            Console.WriteLine(HttpUtility.HtmlDecode(htmlMessage));
+            Console.WriteLine("---------");
+            return Task.CompletedTask;
+        }
+    }
+}
