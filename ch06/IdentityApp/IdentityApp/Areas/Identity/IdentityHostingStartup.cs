4b6d15e54ec76fdbc03591272d1c0d0d480e639e
David Stevenson <drstevenson1958@yahoo.com>
2023-10-30 16:36:12 -0400

    Listing 6-11. Scaffolding an Identity UI Razor Page
    LIsting 6-12. Applying a Partial View in the Register.cshtml file in the Areas/Identiy/Pages/Account folder

---
+++ ch06/IdentityApp/IdentityApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,12 @@
+﻿namespace IdentityApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
