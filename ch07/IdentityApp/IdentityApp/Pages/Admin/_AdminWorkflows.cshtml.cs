3c9404ae5e3bdd3e04651fe118a6e4bcdc613c61
David Stevenson <drstevenson1958@yahoo.com>
2023-10-31 10:57:20 -0400

    Listing 7-6. The Contents of the _AdminWorkflows.cshtml file in the Pages/Identity/Admin folder

---
+++ ch07/IdentityApp/IdentityApp/Pages/Admin/_AdminWorkflows.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Admin
+{
+    public class _AdminWorkflowsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
