19ff4b8f9a2ff12e2d169481eca3b09ff2cb3455
David Stevenson <drstevenson1958@yahoo.com>
2023-10-31 10:39:38 -0400

    Listing 7-4. The Contents of the _Workflows.cshtml file in the Pages/Identity folder

---
+++ ch07/IdentityApp/IdentityApp/Pages/Identity/_Workflows.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Identity
+{
+    public class _WorkflowsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
