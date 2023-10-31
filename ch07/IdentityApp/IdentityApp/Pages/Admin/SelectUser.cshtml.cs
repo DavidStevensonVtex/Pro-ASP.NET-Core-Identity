367f859406f960b6212b46bcb2c76750fdbc92b3
David Stevenson <drstevenson1958@yahoo.com>
2023-10-31 14:01:47 -0400

    Listing 7-21. The Contents of the SelectUser.cshtml.cs file in the Pages/Identity/Admin folder

--- ch07/IdentityApp/IdentityApp/Pages/Admin/SelectUser.cshtml.cs
+++ ch07/IdentityApp/IdentityApp/Pages/Admin/SelectUser.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,29 @@ namespace IdentityApp.Pages.Admin
 {
     public class SelectUserModel : PageModel
     {
+        public SelectUserModel(UserManager<IdentityUser> mgr) =>
+            UserManager = mgr;
+
+        public UserManager<IdentityUser> UserManager { get; set; }
+        public IEnumerable<IdentityUser> Users { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Label { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Callback { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Filter { get; set; }
+
         public void OnGet()
         {
+            Users = UserManager.Users
+                .Where(u => Filter == null || u.Email.Contains(Filter))
+                .OrderBy(u => u.Email)
+                .ToList();
         }
+
+        public IActionResult OnPost() => RedirectToPage(new { Filter, Callback });
     }
 }
