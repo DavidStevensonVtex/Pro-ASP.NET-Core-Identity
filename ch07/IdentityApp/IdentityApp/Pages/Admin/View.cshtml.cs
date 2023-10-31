282524091d937e8aedae67a9f25500813a94d9d4
David Stevenson <drstevenson1958@yahoo.com>
2023-10-31 14:40:08 -0400

    Listing 7-24. The Contents of the View.cshtml.cs file in the Pages/Identity/Admin folder

--- ch07/IdentityApp/IdentityApp/Pages/Admin/View.cshtml.cs
+++ ch07/IdentityApp/IdentityApp/Pages/Admin/View.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,30 @@ namespace IdentityApp.Pages.Admin
 {
     public class ViewModel : PageModel
     {
-        public void OnGet()
+        public ViewModel(UserManager<IdentityUser> mgr) => UserManager = mgr;
+
+        public UserManager<IdentityUser> UserManager { get; set; }
+
+        public IdentityUser IdentityUser { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+
+        public IEnumerable<string> PropertyNames =>
+            typeof(IdentityUser).GetProperties().Select(prop => prop.Name);
+
+        public string GetValue(string name) =>
+            typeof(IdentityUser).GetProperty(name).GetValue(IdentityUser)?.ToString();
+
+        public async Task<IActionResult> OnGetAsync()
         {
+            if (string.IsNullOrEmpty(Id))
+            {
+                return RedirectToPage("Selectuser", new { Label = "View User", Callback = "View" });
+            }
+
+            IdentityUser = await UserManager.FindByIdAsync(Id);
+            return Page();
         }
     }
 }
