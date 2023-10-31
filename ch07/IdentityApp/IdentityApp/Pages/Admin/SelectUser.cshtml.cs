9d397267ea9e83d71b0122349df2859b3930c9ce
David Stevenson <drstevenson1958@yahoo.com>
2023-10-31 13:54:53 -0400

    Listing 7-20. The Contents of the SelectUser.cshtml file in the Pages/Identity/Admin folder

---
+++ ch07/IdentityApp/IdentityApp/Pages/Admin/SelectUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Admin
+{
+    public class SelectUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
