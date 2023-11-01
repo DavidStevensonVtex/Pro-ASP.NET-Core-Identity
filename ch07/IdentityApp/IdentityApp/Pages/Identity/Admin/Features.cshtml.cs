d1941492fe86b22fc0dc72c09fb3c7f56042bbc2
David Stevenson <drstevenson1958@yahoo.com>
2023-11-01 15:49:50 -0400

    Listing 7-31. The Contents of the Features.cshtml file in the Pages/Identity/Admin folder

---
+++ ch07/IdentityApp/IdentityApp/Pages/Identity/Admin/Features.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Identity.Admin
+{
+    public class FeaturesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
