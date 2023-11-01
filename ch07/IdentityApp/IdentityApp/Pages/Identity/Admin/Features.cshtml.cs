a16c1a01d899563c2a5a66dfc21ed3ac6359916b
David Stevenson <drstevenson1958@yahoo.com>
2023-11-01 16:42:36 -0400

    Listing 7-32. The Contents of the Fetaures.cshtml.cs file in the Pages/Identity/Admin folder

--- ch07/IdentityApp/IdentityApp/Pages/Identity/Admin/Features.cshtml.cs
+++ ch07/IdentityApp/IdentityApp/Pages/Identity/Admin/Features.cshtml.cs
@@ -1,12 +1,25 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
 
 namespace IdentityApp.Pages.Identity.Admin
 {
     public class FeaturesModel : PageModel
     {
+        public FeaturesModel(UserManager<IdentityUser> mgr) =>
+            UserManager = mgr;
+
+        public UserManager<IdentityUser> UserManager { get; set; }
+
+        public IEnumerable<(string, string)> Features { get; set; }
+
         public void OnGet()
         {
+            Features = UserManager.GetType().GetProperties()
+                .Where(prop => prop.Name.StartsWith("Supports"))
+                .OrderBy(p => p.Name)
+                .Select(prop => (prop.Name, prop.GetValue(UserManager).ToString()));
         }
     }
 }
